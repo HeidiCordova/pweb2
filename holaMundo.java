6604620397fe0db17bfe1697d78dfea538720c1d
HeidiCordova <hcordovas@unsa.edu.pe>
2022-04-28 03:40:24 +0000

    probando compilador java con el hola mundo

---
+++ holaMundo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class holaMundo {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    //hola mundo
+    System.out.println("Hola Mundo");
+  }
+}
+
